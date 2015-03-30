dcd4f9477e7926d6557144762a026c0cc926629f
patryk-g <patrykgurgul.it@gmail.com>
2015-03-30 15:57:35 +0200

    gruntfile update

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,42 +7,15 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       prod: {
-        // expand: true,
-        src: ['src/js/*.js',
-              'src/js/directives/**/*.js',
-              'src/js/services/*.js',
-              'src/js/controllers/*.js'],
+        src: [
+          'src/js/*.js',
+          'src/js/directives/**/*.js',
+          'src/js/services/*.js',
+          'src/js/controllers/*.js'
+        ],
         dest: 'build/js/scripts.js'
       }
-      // files: {
-      //   expand: true,
-      //   cwd: 'src/js',
-      //   src: ['**/*.js', '!**/*.min.js'],
-      //   dest: 'src/js/min/',
-      //   rename: function(dest, src) {
-      //     var folder = src.substring(0, src.lastIndexOf('/'));
-      //     var filename = src.substring(src.lastIndexOf('/'), src.length);
-      //     filename = filename.substring(0, filename.lastIndexOf('.'));
-      //     return dest + folder + filename + '.min.js';
-      //   }
-      // }
     },
-    // concat: {
-    //   options: {
-    //     separator: ';'
-    //   },
-    //   build: {
-    //     files: {
-    //       'build/js/scripts.js': [
-    //                               'src/js/min/app.min.js',
-    //                               'src/js/min/routes.min.js',
-    //                               'src/js/min/services/*.min.js',
-    //                               'src/js/min/controllers/*.min.js',
-    //                               'src/js/min/directives/*.min.js'
-    //                               ]
-    //     }
-    //   }
-    // },
     copy: {
       prod: {
         files: [
@@ -53,17 +26,18 @@ module.exports = function(grunt) {
             dest: 'build/',
             filter: 'isFile'
           },
-          // {
-          //   expand: true,
-          //   cwd: 'src/templates/',
-          //   src: ['*.html'],
-          //   dest: 'build/templates/',
-          //   filter: 'isFile'
-          // },
           {
             expand: true,
             cwd: 'src/libs/',
-            src: ['**/*.min.js', '**/*.min.css', '**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff', '**/*.woff2'],
+            src: [
+              '**/*.min.js',
+              '**/*.min.css',
+              '**/*.eot',
+              '**/*.svg',
+              '**/*.ttf',
+              '**/*.woff',
+              '**/*.woff2'
+            ],
             dest: 'build/libs',
             filter: 'isFile'
           },
@@ -101,10 +75,11 @@ module.exports = function(grunt) {
           appRoot: 'build/'
         },
         files: {
-          'build/index.html': [ 'build/libs/jquery/**/*.min.js',
-                                'build/libs/angular/**/*.min.js',
-                                'build/libs/**/*.min.js',
-                                'build/js/scripts.js' ]
+          'build/index.html': [
+            'build/libs/jquery/**/*.min.js',
+            'build/libs/angular/**/*.min.js',
+            'build/libs/**/*.min.js',
+            'build/js/scripts.js' ]
         }
       },
       dev: {
@@ -116,13 +91,13 @@ module.exports = function(grunt) {
         },
         files: {
           'src/index.html': [
-              'src/libs/jquery/**/*.min.js',
-              'src/libs/angular/**/*.min.js',
-              'src/libs/**/*.min.js',
-              'src/js/*.js',
-              'src/js/services/*.js',
-              'src/js/directives/**/*.js',
-              'src/js/controllers/*.js'
+            'src/libs/jquery/**/*.min.js',
+            'src/libs/angular/**/*.min.js',
+            'src/libs/**/*.min.js',
+            'src/js/*.js',
+            'src/js/services/*.js',
+            'src/js/directives/**/*.js',
+            'src/js/controllers/*.js'
           ]
         }
       }
@@ -159,7 +134,17 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('build', ['copy:prod', 'uglify:prod', 'sails-linker:prod', 'less:prod']);
-  grunt.registerTask('dev', ['sails-linker:dev', 'less:dev', 'watch:dev' ]);
+  grunt.registerTask('build', [
+    'copy:prod',
+    'uglify:prod',
+    'sails-linker:prod',
+    'less:prod'
+  ]);
+
+  grunt.registerTask('dev', [
+    'sails-linker:dev',
+    'less:dev',
+    'watch:dev'
+  ]);
 
 };
